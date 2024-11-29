aa9a9d3baacd7c0552cb1292a112cc269e9b2c08
Flavio Silva <fal_silva@hotmail.com>
2024-11-29 10:31:26 -0300

    desativando click nativo do html e ativando modal já pronto

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ const doc = elemento => document.querySelector(elemento)
     //retorna um array com os itens que achou:
 const docAll = elemento => document.querySelectorAll(elemento) 
 
-//Parametros = retorna cada pizza e também o index delas
+//Parametros do array= retorna cada pizza e também o index delas
 pizzaJson.map((item, index) => {
     let pizzaItem = doc('.models .pizza-item').cloneNode(true)
+    //imagem das pizzas
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+
+    //preço da pizza
+    //pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    const pizzaPrice = item.price.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+      pizzaItem.querySelector(".pizza-item--price").innerHTML = `${pizzaPrice}`;
+
+    //nome da pizza
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+
+    //descrição da pizza
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
+    //Desativando a atualização da pagina no momento do click
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault()
+        //modal
+        doc('.pizzaWindowArea').style.display = 'flex'
+    })
+    
+    
     //preencher as informações em pizzaItem
     doc('.pizza-area').append(pizzaItem)
 
